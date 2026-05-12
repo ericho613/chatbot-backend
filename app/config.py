48977804c4fc789cfaff98fb1c74ffb7033dd341
Eric Ho <ericho613@gmail.com>
2026-05-12 14:23:30 -0400

    Modified config.py.  Changed OPENAI_MODEL variable value.

--- app/config.py
+++ app/config.py
@@ -40,7 +40,7 @@ class Settings(BaseSettings):
 
     # OpenAI dev settings
     OPENAI_API_KEY: str | None = None
-    OPENAI_MODEL: str = "gpt-4o-mini"
+    OPENAI_MODEL: str = "gpt-4.1-nano"
     OPENAI_EMBEDDING_MODEL: str = "text-embedding-3-small"
 
     # AWS production settings
